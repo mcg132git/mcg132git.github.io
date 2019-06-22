ca8dd8a74a686dc1f48b13257a6f3f175bd1d38a
mcg132git <mcg132@byui.edu>
2019-06-22 13:49:14 -0600

    weather-site-2019-06-22-1st

--- assignments/lesson-8/js/windchill.js
+++ assignments/lesson-8/js/windchill.js
@@ -2,4 +2,6 @@ var temp = parseInt(document.getElementById('currentTemp').innerHTML);
 
 var windpseed = parseInt(document.getElementById('windSpeed').innerHTML);
 
-var wndchill = 35.74 + 0.6215*temp - 35.75*windpseed^0.16 + 0.4275*temp*(windpseed^0.16);
+var windchill = 35.74 + 0.6215*temp - 35.75*(Math.pow(windpseed,0.16)) + 0.4275*temp*(Math.pow(windpseed,0.16));
+
+document.getElementById("windChill").innerHTML= windchill.toFixed(2);
